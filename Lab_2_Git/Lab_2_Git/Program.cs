847eb478f43b1023a3efac6ae6e9e1fc12b85349
vodachuk <vodachuk@gmail.com>
2017-03-15 16:58:07 +0200

    Project

---
+++ Lab_2_Git/Lab_2_Git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Git
+{
+    class Purse
+    {
+ 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
