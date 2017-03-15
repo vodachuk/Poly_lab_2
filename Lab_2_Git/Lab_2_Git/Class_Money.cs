5922f697150b6f1e053717023845664bae5300fc
Vients <yvients@gmail.com>
2017-03-15 17:34:02 +0200

    Add class Class_Money

---
+++ Lab_2_Git/Lab_2_Git/Class_Money.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Money
+{
+    public int momey;
+    public sting currency;
+
+	public Money(int money)
+	{
+        this.money = money;
+	}
+
+    void GetInformation()
+    {
+        Console.WriteLine("Youn have money = {0}", money);
+    }
+}
