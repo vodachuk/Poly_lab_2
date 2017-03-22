475531df9a19ef77da47ae7f67867242e556fdfb
ihor-chaban <igor.chaban.1995@gmail.com>
2017-03-22 17:15:53 +0200

    merged chaban

--- Lab_2_Git/Lab_2_Git/Program.cs
+++ Lab_2_Git/Lab_2_Git/Program.cs
@@ -10,11 +10,7 @@ namespace Lab_2_Git
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-         
-=======
             CreditCard card = new CreditCard("1234 5678 9012 3456", new DateTime(2017, 8, 21), 536, "USD");
->>>>>>> chaban
         }
     }
 }
